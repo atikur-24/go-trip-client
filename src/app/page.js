b56bd9e0599120ce24652e79070ccb058c95092f
Atikur <atikurrahman7223@gmail.com>
2023-11-27 19:23:28 +0600

    create top navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h2 className="text-3xl text-cyan-500 font-bold text-center py-10">This is Tour & Travel Agency</h2>
+      <h2 className="text-3xl text-red-500 font-bold text-center py-10">This is Tour & Travel Agency</h2>
     </main>
   );
 }
