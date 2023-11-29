bb74c3c7c9bd3e514bd759a9a88a02932c1e986c
Atikur <atikurrahman7223@gmail.com>
2023-11-29 23:55:12 +0600

    fixed banner input field

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,45 +1,46 @@
-import { LocationOnOutlined } from "@mui/icons-material";
+import { GrLocation } from "react-icons/gr";
 
 const Banner = () => {
   return (
     // {/* home */}
     <section className="w-full relative flex items-center justify-center m-auto">
       {/* overlay */}
-      <div className="absolute h-full w-full bg-banner top-0 bottom-0 z-10 mix-blend-hard-light"></div>
+      <div className="absolute h-full w-full bg-banner top-0 bottom-0 z-[1] mix-blend-hard-light"></div>
       {/* className="h-full object-cover mx-auto w-full" */}
       <video className="absolute h-full w-full top-0 bottom-0 object-cover" src="bannerVid.mp4" type="video/mp4" autoPlay loop muted></video>
       {/* homeContent container */}
-      <div className="w-full h-max pt-32 pb-12 px-6 flex gap-y-12 flex-col items-stretch justify-center m-auto text-white z-10">
-        {/* textDive */}
+      <div className="w-full h-max pt-32 pb-12 px-6 flex gap-y-12 flex-col items-stretch justify-center m-auto text-white z-[100]">
+        {/* textDiv */}
         <div className="py-8 px-0">
           {/* small Text */}
-          <span className="text-sm bg-none border-none outline-none text w-full-sm font-medium placeholder:text-sm">over packages</span>
+          <span className="text-sm uppercase">our packages</span>
           {/* homeTitle */}
-          <h1 className="relative text-4xl font-bold">Search Your Holidays</h1>
+          <h1 className="relative text-3xl font-bold">Search Your Holidays</h1>
         </div>
         {/* cardDiv grid */}
         <div className="relative py-8 px-4 bg-white rounded-lg gap-4">
           {/* destinationInput */}
           <div>
-            <label className="block text-gray-700 pb-3 font-medium text-sm" htmlFor="city">
+            <label className="block text-gray-700 pb-[10px] font-medium text-sm" htmlFor="city">
               Search Your Destination:
             </label>
             {/* input flex */}
             <div className="h-10 py-0 px-4 rounded-full bg-gray-300">
-              <input className="bg-none border-none outline-none w-full text-sm font-medium placeholder:text-sm" type="text" name="text" placeholder="Enter Name Here" />
+              {/* className="bg-none border-none outline-none w-full text-sm font-medium placeholder:text-sm" */}
+              <input type="text" name="text" placeholder="Enter Name Here...." />
               {/* icon */}
-              <LocationOnOutlined />
+              <GrLocation className="text-lg text-gray-800 ml-[10px]" />
             </div>
           </div>
 
           {/* dateInput */}
           <div>
-            <label className="block text-gray-700 pb-3 font-medium text-sm" htmlFor="date">
+            <label className="block text-gray-700 pb-[10px] font-medium text-sm" htmlFor="date">
               Select Your Date:
             </label>
             {/* input flex */}
             <div className="h-10 py-0 px-4 rounded-full bg-gray-300">
-              <input className="bg-none border-none outline-none w-full text-sm font-medium placeholder:text-sm" type="date" name="date" />
+              <input type="date" name="date" />
             </div>
           </div>
 
@@ -47,15 +48,15 @@ const Banner = () => {
           <div>
             {/* label_total flex */}
             <div className="justify-between items-center">
-              <label className="block text-gray-700 pb-3 font-medium text-sm" htmlFor="price">
+              <label className="block text-gray-700 pb-[10px] font-medium text-sm" htmlFor="price">
                 Max Price:
               </label>
               {/* total */}
-              <h3 className="text-gray-700 text-lg pb-3">$5000</h3>
+              <h3 className="text-gray-700 text-lg pb-[10px]">$5000</h3>
             </div>
             {/* input flex */}
             <div className="h-10 py-0 px-4 rounded-full bg-gray-300">
-              <input className="bg-none border-none outline-none w-full text-sm font-medium placeholder:text-sm" type="range" name="price" max="5000" min="1000" />
+              <input type="range" name="price" max="5000" min="1000" />
             </div>
           </div>
         </div>
