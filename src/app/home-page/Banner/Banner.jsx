8f484d156bef084f30db61ee467877242d1ac2b7
atikur-24 <atikurrahman7223@gmail.com>
2024-01-04 23:22:08 +0600

    remove price range in banner section

--- src/app/home-page/Banner/Banner.jsx
+++ src/app/home-page/Banner/Banner.jsx
@@ -8,14 +8,7 @@ const Banner = () => {
       {/* overlay */}
       <div className="absolute h-full w-full bg-banner top-0 bottom-0 z-[1] mix-blend-hard-light"></div>
       {/* className="h-full object-cover mx-auto w-full" */}
-      <video
-        className="absolute h-full w-full top-0 bottom-0 object-cover"
-        src="bannerVid.mp4"
-        type="video/mp4"
-        autoPlay
-        loop
-        muted
-      ></video>
+      <video className="absolute h-full w-full top-0 bottom-0 object-cover" src="bannerVid.mp4" type="video/mp4" autoPlay loop muted></video>
       {/* homeContent container */}
       <div className="w-full h-max pt-32 pb-12 px-6 flex gap-y-12 flex-col items-stretch justify-center m-auto text-white z-[100]">
         {/* textDiv */}
@@ -29,62 +22,18 @@ const Banner = () => {
         <div className="relative py-8 px-4 bg-white rounded-lg gap-4">
           {/* destinationInput */}
           <div>
-            <label
-              className="block text-gray-700 pb-[10px] font-medium text-sm"
-              htmlFor="city"
-            >
+            <label className="block text-gray-700 pb-[10px] font-medium text-sm" htmlFor="city">
               Search Your Destination:
             </label>
             {/* input flex */}
             <div className="h-10 py-0 px-4 rounded-full bg-gray-300">
               {/* className="bg-none border-none outline-none w-full text-sm font-medium placeholder:text-sm" */}
-              <input
-                type="text"
-                name="text"
-                placeholder="Enter Name Here...."
-              />
+              <input type="text" name="text" placeholder="Enter Name Here...." />
               {/* icon */}
               <GrLocation className="text-lg text-gray-800 ml-[10px]" />
             </div>
           </div>
-
-          {/* dateInput */}
-          <div>
-            <label
-              className="block text-gray-700 pb-[10px] font-medium text-sm"
-              htmlFor="date"
-            >
-              Select Your Date:
-            </label>
-            {/* input flex */}
-            <div className="h-10 py-0 px-4 rounded-full bg-gray-300">
-              <input type="date" name="date" />
-            </div>
-          </div>
-
-          {/* priceInput */}
-          <div>
-            {/* label_total flex */}
-            <div className="justify-between items-center">
-              <label
-                className="block text-gray-700 pb-[10px] font-medium text-sm"
-                htmlFor="price"
-              >
-                Max Price:
-              </label>
-              {/* total */}
-              <h3 className="text-gray-700 text-lg pb-[10px]">$5000</h3>
-            </div>
-            {/* input flex */}
-            <div className="h-10 py-0 px-4 rounded-full bg-gray-300">
-              <input type="range" name="price" max="5000" min="1000" />
-            </div>
-          </div>
         </div>
-        {/* <div className="flex gap-4">
-          <Facebook />
-          <Instagram />
-        </div> */}
       </div>
     </section>
   );
